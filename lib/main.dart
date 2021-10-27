562956664b2a24f86620865463c1172724b0abfb
YourFoxy <Foxyanzh09@gmail.com>
2021-10-27 09:47:36 +0300

    2 commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,10 +12,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   // SharedPreferences.setMockInitialValues({});
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -70,7 +70,16 @@ class _HomePageWidgetsState extends State<HomePageWidgets> {
           actions: _appBarMenuWidget(Theme.of(context).primaryColor)),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
-        onPressed: () => AddingCategory(context: context).add(),
+        onPressed: () {
+          showDialog(
+              barrierColor: Colors.black54,
+              context: context,
+              builder: (context) {
+                return AddingCategoryWidget2();
+              });
+        },
+        // AddingCategory().add(context),
+        //AddingCategory(context: context).add(),
         child: const Icon(Icons.add),
       ),
       bottomNavigationBar: _bottomMenu(),
@@ -85,19 +94,154 @@ class _HomePageWidgetsState extends State<HomePageWidgets> {
 }
 
 class AddingCategory {
-  final BuildContext context;
+  //final BuildContext context;
 
-  AddingCategory({
-    required this.context,
-  });
+  // AddingCategory({
+  //   required this.context,
+  // });
 
-  void add() {
+  void add(BuildContext context) {
+    print('d');
     showDialog(
         barrierColor: Colors.black54,
         context: context,
         builder: (context) {
           return const AddingCategoryWidget();
         });
+  }
+}
+
+class AddingCategoryWidget2 extends StatefulWidget {
+  AddingCategoryWidget2({Key? key}) : super(key: key);
+
+  @override
+  _AddingCategoryWidget2State createState() => _AddingCategoryWidget2State();
+}
+
+class _AddingCategoryWidget2State extends State<AddingCategoryWidget2> {
+  final TextEditingController _controller = TextEditingController();
+
+  Widget _actionText() {
+    return const Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Align(
+          alignment: Alignment.topCenter,
+          child: TextParameters(
+            text: 'creating a category',
+            fontSize: 30.0,
+          )),
+    );
+  }
+
+  Widget _inputField() {
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        width: 250,
+        child: TextField(
+          autofocus: true,
+          controller: _controller,
+          style: const TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+          ),
+          decoration: InputDecoration(
+            hintStyle: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+              color: Colors.white30,
+            ),
+            hintText: 'Name of new category',
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Colors.white,
+                width: 3,
+              ),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(30),
+              borderSide: const BorderSide(
+                color: Colors.white,
+                width: 1,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void add() {
+    if (_controller.text != '') {
+      myR.add({'myId': _controller.text});
+      _controller.text = '';
+      Navigator.pop(context);
+    }
+  }
+
+  Widget _allButtons() {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            _button('CANCEL', () {
+              Navigator.pop(context);
+            }),
+            Container(
+              width: 3,
+              height: 20,
+              color: Colors.white,
+            ),
+            _button('SAVE', add),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _button(String buttonType, Function() buttonFunction) {
+    return Flexible(
+      //flex: 1,
+      child: Center(
+        // ignore: deprecated_member_use
+        child: FlatButton(
+          child: TextParameters(
+            text: buttonType,
+            fontSize: 20.0,
+          ),
+          onPressed: () => buttonFunction(),
+        ),
+      ),
+    );
+  }
+
+  Widget _addWindow() {
+    return SizedBox(
+      height: 200,
+      width: 240,
+      child: Column(
+        children: <Widget>[
+          _actionText(),
+          _inputField(),
+          _allButtons(),
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      backgroundColor: Theme.of(context).primaryColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+      elevation: 16,
+      child: _addWindow(),
+    );
+    ;
   }
 }
 
@@ -151,7 +295,7 @@ class _AddingCategoryWidgetState extends State<AddingCategoryWidget> {
     );
   }
 
-  Widget _addWindow(BuildContext context) {
+  Widget _addWindow() {
     return SizedBox(
       height: 200,
       width: 240,
@@ -229,7 +373,7 @@ class _AddingCategoryWidgetState extends State<AddingCategoryWidget> {
       backgroundColor: Theme.of(context).primaryColor,
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
       elevation: 16,
-      child: _addWindow(context),
+      child: _addWindow(),
     );
   }
 }
