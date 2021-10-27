1a004db40a12538ee5507866995ede4aa9663124
YourFoxy <Foxyanzh09@gmail.com>
2021-10-27 10:08:50 +0300

    3 commit

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -75,7 +75,7 @@ class _HomePageWidgetsState extends State<HomePageWidgets> {
               barrierColor: Colors.black54,
               context: context,
               builder: (context) {
-                return AddingCategoryWidget2();
+                return const AddingCategoryWidget();
               });
         },
         // AddingCategory().add(context),
@@ -111,14 +111,14 @@ class AddingCategory {
   }
 }
 
-class AddingCategoryWidget2 extends StatefulWidget {
-  AddingCategoryWidget2({Key? key}) : super(key: key);
+class AddingCategoryWidget extends StatefulWidget {
+  const AddingCategoryWidget({Key? key}) : super(key: key);
 
   @override
-  _AddingCategoryWidget2State createState() => _AddingCategoryWidget2State();
+  _AddingCategoryWidgetState createState() => _AddingCategoryWidgetState();
 }
 
-class _AddingCategoryWidget2State extends State<AddingCategoryWidget2> {
+class _AddingCategoryWidgetState extends State<AddingCategoryWidget> {
   final TextEditingController _controller = TextEditingController();
 
   Widget _actionText() {
@@ -242,139 +242,6 @@ class _AddingCategoryWidget2State extends State<AddingCategoryWidget2> {
       child: _addWindow(),
     );
     ;
-  }
-}
-
-class AddingCategoryWidget extends StatefulWidget {
-  const AddingCategoryWidget({Key? key}) : super(key: key);
-
-  @override
-  _AddingCategoryWidgetState createState() => _AddingCategoryWidgetState();
-}
-
-class _AddingCategoryWidgetState extends State<AddingCategoryWidget> {
-  final TextEditingController _controller = TextEditingController();
-
-  Widget _inputField() {
-    return Align(
-      alignment: Alignment.center,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        width: 250,
-        child: TextField(
-          autofocus: true,
-          controller: _controller,
-          style: const TextStyle(
-            fontSize: 20,
-            color: Colors.white,
-          ),
-          decoration: InputDecoration(
-            hintStyle: const TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 20,
-              color: Colors.white30,
-            ),
-            hintText: 'Name of new category',
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-              borderSide: const BorderSide(
-                color: Colors.white,
-                width: 3,
-              ),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
-              borderSide: const BorderSide(
-                color: Colors.white,
-                width: 1,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _addWindow() {
-    return SizedBox(
-      height: 200,
-      width: 240,
-      child: Column(
-        children: <Widget>[
-          _actionText(),
-          _inputField(),
-          _allButtons(context),
-        ],
-      ),
-    );
-  }
-
-  void add() {
-    if (_controller.text != '') {
-      myR.add({'myId': _controller.text});
-      _controller.text = '';
-      Navigator.pop(context);
-    }
-  }
-
-  Widget _allButtons(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          children: <Widget>[
-            _button('CANCEL', () {
-              Navigator.pop(context);
-            }),
-            Container(
-              width: 3,
-              height: 20,
-              color: Colors.white,
-            ),
-            _button('SAVE', add)
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _button(String buttonType, Function buttonFunction) {
-    return Flexible(
-      flex: 1,
-      child: Center(
-        // ignore: deprecated_member_use
-        child: FlatButton(
-          child: TextParameters(
-            text: buttonType,
-            fontSize: 20.0,
-          ),
-          onPressed: buttonFunction(),
-        ),
-      ),
-    );
-  }
-
-  Widget _actionText() {
-    return const Padding(
-      padding: EdgeInsets.only(top: 20),
-      child: Align(
-          alignment: Alignment.topCenter,
-          child: TextParameters(
-            text: 'creating a category',
-            fontSize: 30.0,
-          )),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      backgroundColor: Theme.of(context).primaryColor,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
-      elevation: 16,
-      child: _addWindow(),
-    );
   }
 }
 
@@ -525,7 +392,7 @@ class _CategoryWidget2State extends State<CategoryWidget2> {
     );
   }
 
-  Widget _removeCategory(Function buttonRemove) {
+  Widget _removeCategory(Function() buttonRemove) {
     return Align(
       alignment: Alignment.topRight,
       child: Padding(
@@ -534,7 +401,7 @@ class _CategoryWidget2State extends State<CategoryWidget2> {
           heightFactor: .2,
           widthFactor: .2,
           child: InkWell(
-            onTap: buttonRemove(),
+            onTap: () => buttonRemove(),
             child: const Icon(
               Icons.disabled_by_default_outlined,
               color: Colors.white,
@@ -575,6 +442,7 @@ class _CategoryWidget2State extends State<CategoryWidget2> {
     return StreamBuilder(
         stream: myR.orderBy('myId').snapshots(),
         builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          print('ddddddddddddddddddddddddddddddddddddddddddd');
           if (snapshot.data == null) return const CircularProgressIndicator();
           return Expanded(
             child: Padding(
